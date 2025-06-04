0d7cacacfc21660d882e8ce5d8e99e880cd0cd9c
Callum-22169408 <wns6192@autuni.ac.nz>
2025-06-04 16:41:16 +1200

    First Commit Testing!
    Second commit testing
    
    Second commit testing
    
    Second commit testing
    
    for loop test time!!

---
+++ src/pdc_project_2/PDC_Project_2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdc_project_2;
+
+/**
+ *
+ * @author User
+ */
+public class PDC_Project_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello!");
+        System.out.println("Testing testing commit ");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
