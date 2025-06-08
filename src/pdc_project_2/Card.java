f4c8ed01a39544dc6e2572b1007d2cfa0136c086
Callum-22169408 <wns6192@autuni.ac.nz>
2025-06-08 14:32:15 +1200

    Card class completed

--- src/pdc_project_2/Card.java
+++ src/pdc_project_2/Card.java
@@ -8,6 +8,47 @@ package pdc_project_2;
  *
  * @author User
  */
+//Card class is building the deck of cards 
 public class Card {
     
+        private final String rank; 
+        private final String suit; 
+    
+//Constructor for the deck
+public Card(String rank, String suit){
+     this.rank = rank; 
+     this.suit = suit; 
+    }
+
+//Returns the value of the face and ace cards e.g face =10 ace =11
+public int getBlackJackValue (){
+    switch(rank){
+        case "J":
+        case "Q":
+        case "K":
+          return 10; 
+        case "A": 
+            return 11; 
+        default: 
+            return Integer.parseInt(rank);
+      }
+   }
+
+   //Is it an ace or not? that's the real question. 
+   public boolean isAce (){
+       return "A".equals(rank);
+   }
+   
+    // returns what the card actually is 
+   @Override
+   public String toString(){
+       return rank + "-" + suit; 
+   }
+   
+   //Gets the image files for the cards 
+   public String getImageFilePath(){
+       return String.format("./cards/%s.png", toString());
+   }
 }
+
+    
